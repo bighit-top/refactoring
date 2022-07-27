166fdf395be98332231a8646c2a92a898b531364
topKim <bighit.top@gmail.com>
2022-07-27 16:54:46 +0900

    2. duplicated code

--- src/main/java/me/whiteship/refactoring/_02_duplicated_code/_04_extract_function/StudyDashboard.java
+++ src/main/java/me/whiteship/refactoring/_02_duplicated_code/_04_extract_function/StudyDashboard.java
@@ -12,30 +12,53 @@ public class StudyDashboard {
 
     private void printParticipants(int eventId) throws IOException {
         // Get github issue to check homework
-        GitHub gitHub = GitHub.connect();
-        GHRepository repository = gitHub.getRepository("whiteship/live-study");
-        GHIssue issue = repository.getIssue(eventId);
+//        GitHub gitHub = GitHub.connect();
+//        GHRepository repository = gitHub.getRepository("whiteship/live-study");
+//        GHIssue issue = repository.getIssue(eventId);
+        GHIssue issue = getGhIssue(eventId);
 
         // Get participants
-        Set<String> participants = new HashSet<>();
-        issue.getComments().forEach(c -> participants.add(c.getUserName()));
+//        Set<String> participants = new HashSet<>();
+//        issue.getComments().forEach(c -> participants.add(c.getUserName()));
+        Set<String> participants = getUsernames(issue);
 
         // Print participants
-        participants.forEach(System.out::println);
+//        participants.forEach(System.out::println);
+        print(participants);
     }
 
     private void printReviewers() throws IOException {
         // Get github issue to check reviews
-        GitHub gitHub = GitHub.connect();
-        GHRepository repository = gitHub.getRepository("whiteship/live-study");
-        GHIssue issue = repository.getIssue(30);
+//        GitHub gitHub = GitHub.connect();
+//        GHRepository repository = gitHub.getRepository("whiteship/live-study");
+//        GHIssue issue = repository.getIssue(30);
+        GHIssue issue = getGhIssue(30);
 
         // Get reviewers
-        Set<String> reviewers = new HashSet<>();
-        issue.getComments().forEach(c -> reviewers.add(c.getUserName()));
+//        Set<String> reviewers = new HashSet<>();
+//        issue.getComments().forEach(c -> reviewers.add(c.getUserName()));
+        Set<String> reviewers = getUsernames(issue);
 
         // Print reviewers
-        reviewers.forEach(System.out::println);
+//        reviewers.forEach(System.out::println);
+        print(reviewers);
+    }
+
+    private void print(Set<String> participants) {
+        participants.forEach(System.out::println);
+    }
+
+    private Set<String> getUsernames(GHIssue issue) throws IOException {
+        Set<String> usernames = new HashSet<>();
+        issue.getComments().forEach(c -> usernames.add(c.getUserName()));
+        return usernames;
+    }
+
+    private GHIssue getGhIssue(int eventId) throws IOException {
+        GitHub gitHub = GitHub.connect();
+        GHRepository repository = gitHub.getRepository("whiteship/live-study");
+        GHIssue issue = repository.getIssue(eventId);
+        return issue;
     }
 
     public static void main(String[] args) throws IOException {
