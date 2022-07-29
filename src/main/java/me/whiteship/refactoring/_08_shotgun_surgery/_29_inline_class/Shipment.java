b9387455068c853941eddee26e45a36b2aa6d46b
topKim <bighit.top@gmail.com>
2022-07-29 18:59:29 +0900

    8. shotgun surgery

--- src/main/java/me/whiteship/refactoring/_08_shotgun_surgery/_29_inline_class/Shipment.java
+++ src/main/java/me/whiteship/refactoring/_08_shotgun_surgery/_29_inline_class/Shipment.java
@@ -2,21 +2,55 @@ package me.whiteship.refactoring._08_shotgun_surgery._29_inline_class;
 
 public class Shipment {
 
-    private TrackingInformation trackingInformation;
+//    private TrackingInformation trackingInformation;
 
-    public Shipment(TrackingInformation trackingInformation) {
-        this.trackingInformation = trackingInformation;
+    private String shippingCompany;
+    private String trackingNumber;
+
+//    public Shipment(TrackingInformation trackingInformation) {
+//        this.trackingInformation = trackingInformation;
+//    }
+
+    //생성자
+    public Shipment(String shippingCompany, String trackingNumber) {
+        this.shippingCompany = shippingCompany;
+        this.trackingNumber = trackingNumber;
     }
 
-    public TrackingInformation getTrackingInformation() {
-        return trackingInformation;
-    }
+//    public TrackingInformation getTrackingInformation() {
+//        return trackingInformation;
+//    }
 
-    public void setTrackingInformation(TrackingInformation trackingInformation) {
-        this.trackingInformation = trackingInformation;
-    }
+//    public void setTrackingInformation(TrackingInformation trackingInformation) {
+//        this.trackingInformation = trackingInformation;
+//    }
 
+//    public String getTrackingInfo() {
+//        return this.trackingInformation.display();
+//    }
     public String getTrackingInfo() {
-        return this.trackingInformation.display();
+        return this.shippingCompany + ": " + this.trackingNumber;
     }
+
+//    public TrackingInformation(String shippingCompany, String trackingNumber) {
+//        this.shippingCompany = shippingCompany;
+//        this.trackingNumber = trackingNumber;
+//    }
+
+    public String getShippingCompany() {
+        return shippingCompany;
+    }
+
+    public void setShippingCompany(String shippingCompany) {
+        this.shippingCompany = shippingCompany;
+    }
+
+    public String getTrackingNumber() {
+        return trackingNumber;
+    }
+
+    public void setTrackingNumber(String trackingNumber) {
+        this.trackingNumber = trackingNumber;
+    }
+
 }
