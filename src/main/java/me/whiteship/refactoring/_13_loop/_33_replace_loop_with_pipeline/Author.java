d5a642bbab64f9cd878d681adfabdb846898f04e
topKim <bighit.top@gmail.com>
2022-07-31 00:10:40 +0900

    13. loop

--- src/main/java/me/whiteship/refactoring/_13_loop/_33_replace_loop_with_pipeline/Author.java
+++ src/main/java/me/whiteship/refactoring/_13_loop/_33_replace_loop_with_pipeline/Author.java
@@ -1,7 +1,7 @@
 package me.whiteship.refactoring._13_loop._33_replace_loop_with_pipeline;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Author {
 
@@ -15,15 +15,22 @@ public class Author {
     }
 
     static public List<String> TwitterHandles(List<Author> authors, String company) {
-        var result = new ArrayList<String> ();
-        for (Author a : authors) {
-            if (a.company.equals(company)) {
-                var handle = a.twitterHandle;
-                if (handle != null)
-                    result.add(handle);
-            }
-        }
-        return result;
+//        var result = new ArrayList<String> ();
+//        for (Author a : authors) {
+//            if (a.company.equals(company)) {
+//                var handle = a.twitterHandle;
+//                if (handle != null)
+//                    result.add(handle);
+//            }
+//        }
+//        return result;
+
+        return authors.stream() //for (Author a : authors) {
+                .filter(author -> author.company.equals(company)) //if (a.company.equals(company)) {
+                .map(author -> author.twitterHandle) //var handle = a.twitterHandle;
+                .filter(t -> t != null) //if (handle != null)
+                .collect(Collectors.toList());//result.add(handle); }}
+
     }
 
 }
