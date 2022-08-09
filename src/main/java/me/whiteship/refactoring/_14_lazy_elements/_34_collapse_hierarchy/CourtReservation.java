8323df2ab4f8ea7b69f16eb867e37ab3bb6fb05c
topKim <bighit.top@gmail.com>
2022-08-09 17:39:41 +0900

    14.lazy element

--- src/main/java/me/whiteship/refactoring/_14_lazy_elements/_34_collapse_hierarchy/CourtReservation.java
+++ src/main/java/me/whiteship/refactoring/_14_lazy_elements/_34_collapse_hierarchy/CourtReservation.java
@@ -2,4 +2,6 @@ package me.whiteship.refactoring._14_lazy_elements._34_collapse_hierarchy;
 
 public class CourtReservation extends Reservation {
 
+//    private String courtNumber;
+
 }
--- src/main/java/me/whiteship/refactoring/_14_lazy_elements/_34_collapse_hierarchy/Reservation.java
+++ src/main/java/me/whiteship/refactoring/_14_lazy_elements/_34_collapse_hierarchy/Reservation.java
@@ -16,4 +16,5 @@ public class Reservation {
     private boolean paid;
 
     private String courtNumber;
+
 }
