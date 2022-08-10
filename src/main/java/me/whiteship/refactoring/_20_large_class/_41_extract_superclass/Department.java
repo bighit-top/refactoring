1df293414acc23aa751cef65a1557d3da4b1252d
topKim <bighit.top@gmail.com>
2022-08-10 12:28:20 +0900

    20. large class

--- src/main/java/me/whiteship/refactoring/_20_large_class/_41_extract_superclass/Department.java
+++ src/main/java/me/whiteship/refactoring/_20_large_class/_41_extract_superclass/Department.java
@@ -2,27 +2,36 @@ package me.whiteship.refactoring._20_large_class._41_extract_superclass;
 
 import java.util.List;
 
-public class Department {
+//public class Department {
+public class Department extends Party {
 
-    private String name;
+//    private String name;
 
     private List<Employee> staff;
 
-    public String getName() {
-        return name;
+    public Department(String name) {
+        super(name);
     }
+
+//    public String getName() {
+//        return name;
+//    }
+
 
     public List<Employee> getStaff() {
         return staff;
     }
 
-    public double totalMonthlyCost() {
-        return this.staff.stream().mapToDouble(e -> e.getMonthlyCost()).sum();
+//    public double totalMonthlyCost() {
+    @Override
+    public double monthlyCost() {
+        return this.staff.stream().mapToDouble(e -> e.monthlyCost()).sum();
     }
 
-    public double totalAnnualCost() {
-        return this.totalMonthlyCost() * 12;
-    }
+//    public double totalAnnualCost() {
+//    public double annualCost() {
+//        return this.monthlyCost() * 12;
+//    }
 
     public int headCount() {
         return this.staff.size();
