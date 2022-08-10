52628b8b042f92257b3ce8fecd063fb6ffa90096
topKim <bighit.top@gmail.com>
2022-08-10 12:29:26 +0900

    delete comment: 20.large class

--- src/main/java/me/whiteship/refactoring/_20_large_class/_41_extract_superclass/Department.java
+++ src/main/java/me/whiteship/refactoring/_20_large_class/_41_extract_superclass/Department.java
@@ -2,10 +2,7 @@ package me.whiteship.refactoring._20_large_class._41_extract_superclass;
 
 import java.util.List;
 
-//public class Department {
 public class Department extends Party {
-
-//    private String name;
 
     private List<Employee> staff;
 
@@ -13,25 +10,14 @@ public class Department extends Party {
         super(name);
     }
 
-//    public String getName() {
-//        return name;
-//    }
-
-
     public List<Employee> getStaff() {
         return staff;
     }
 
-//    public double totalMonthlyCost() {
     @Override
     public double monthlyCost() {
         return this.staff.stream().mapToDouble(e -> e.monthlyCost()).sum();
     }
-
-//    public double totalAnnualCost() {
-//    public double annualCost() {
-//        return this.monthlyCost() * 12;
-//    }
 
     public int headCount() {
         return this.staff.size();
--- src/main/java/me/whiteship/refactoring/_20_large_class/_41_extract_superclass/Employee.java
+++ src/main/java/me/whiteship/refactoring/_20_large_class/_41_extract_superclass/Employee.java
@@ -1,11 +1,8 @@
 package me.whiteship.refactoring._20_large_class._41_extract_superclass;
 
-//public class Employee {
 public class Employee extends Party {
 
     private Integer id;
-
-//    private String name;
 
     private double monthlyCost;
 
@@ -13,19 +10,10 @@ public class Employee extends Party {
         super(name);
     }
 
-//    public double annualCost() {
-//        return this.monthlyCost * 12;
-//    }
-
     public Integer getId() {
         return id;
     }
 
-//    public String getName() {
-//        return name;
-//    }
-
-//    public double getMonthlyCost() {
     @Override
     public double monthlyCost() {
         return monthlyCost;
